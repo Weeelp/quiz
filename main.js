3cd854f389a093e61b727fc7086b66c1e6e2864c
Elizaveta <e.nesterovich007@gmail.com>
2024-03-29 00:08:41 +0300

    🤗

---
+++ main.js
@@ -0,0 +1,164 @@
+const option1 = document.querySelector(".option1");
+const option2 = document.querySelector(".option2");
+const option3 = document.querySelector(".option3");
+const option4 = document.querySelector(".option4");
+
+const optionElements = document.querySelectorAll(".option");
+
+const question = document.getElementById("question");
+
+const NumberOfQuestion = document.getElementById("number-of-question");
+const numberOfAllQuestions = document.getElementById("number-of-all-questions");
+
+let indexOfQuestion;
+let indexOfPage = 0;
+
+const answersTracker = document.getElementById("answers-tracker");
+const btnNext = document.getElementById("btn-next");
+
+let score = 0;
+
+const correctAnswer = document.getElementById("correct-answer");
+const numberOfAllQuestions2 = document.getElementById(
+  "number-of-all-questions-2"
+);
+const btnTryAgain = document.getElementById("btn-try-again");
+
+const questions = [
+  {
+    question: "1?",
+    options: ["1", "2", "3", "4"],
+    rightAnswer: 1,
+  },
+  {
+    question: "2?",
+    options: ["1", "3", "4", "5"],
+    rightAnswer: 2,
+  },
+  {
+    question: "3?",
+    options: ["5", "6", "7", "8"],
+    rightAnswer: 0,
+  },
+  {
+    question: "10",
+    options: ["1", "2", "3", "4"],
+    rightAnswer: 1,
+  },
+];
+
+numberOfAllQuestions.innerHTML = questions.length;
+
+const load = () => {
+  question.innerHTML = questions[indexOfQuestion].question;
+
+  option1.innerHTML = questions[indexOfQuestion].options[0];
+  option2.innerHTML = questions[indexOfQuestion].options[1];
+  option3.innerHTML = questions[indexOfQuestion].options[2];
+  option4.innerHTML = questions[indexOfQuestion].options[3];
+
+  NumberOfQuestion.innerHTML = indexOfPage + 1;
+  indexOfPage++;
+};
+
+let completedAnswers = [];
+
+const randomQuestion = () => {
+  let randomNumber = Math.floor(Math.random() * questions.length);
+  let hitDuplicate = false;
+
+  if (indexOfPage == questions.length) {
+    quizOver();
+  } else {
+    if (completedAnswers.length > 0) {
+      completedAnswers.forEach((item) => {
+        if (item == randomNumber) {
+          hitDuplicate = true;
+        }
+      });
+      if (hitDuplicate) {
+        randomQuestion();
+      } else {
+        indexOfQuestion = randomNumber;
+        load();
+      }
+    }
+    if (completedAnswers.length == 0) {
+      indexOfQuestion = randomNumber;
+      load();
+    }
+  }
+  completedAnswers.push(indexOfQuestion);
+};
+
+const checkAnswer = (el) => {
+  if (el.target.dataset.id == questions[indexOfQuestion].rightAnswer) {
+    el.target.classList.add("correct");
+    abdateAnswerTracker("correct");
+    score++;
+  } else {
+    el.target.classList.add("wrong");
+    abdateAnswerTracker("wrong");
+  }
+  disabledOptions();
+};
+
+for (option of optionElements) {
+  option.addEventListener("click", (e) => checkAnswer(e));
+}
+
+const disabledOptions = () => {
+  optionElements.forEach((item) => {
+    item.classList.add("disabled");
+    if (item.dataset.id == questions[indexOfQuestion].rightAnswer) {
+      item.classList.add("correct");
+    }
+  });
+};
+
+const enableOptions = () => {
+  optionElements.forEach((item) => {
+    item.classList.remove("disabled", "correct", "wrong");
+  });
+};
+
+const answerTracker = () => {
+  questions.forEach(() => {
+    const div = document.createElement("div");
+    answersTracker.appendChild(div);
+  });
+};
+
+const abdateAnswerTracker = (status) => {
+  answersTracker.children[indexOfPage - 1].classList.add(`${status}`);
+};
+
+const validate = () => {
+  if (optionElements[0].classList.contains("disabled")) {
+    randomQuestion();
+    enableOptions();
+  } else {
+    alert("Выбирай попуск");
+  }
+};
+
+const quizOver = () => {
+  document.querySelector(".quiz-over-modal").classList.add("active");
+  correctAnswer.innerHTML = score;
+  numberOfAllQuestions2, (innerHTML = question.length);
+};
+
+const tryAgain = () => {
+  window.location.reload();
+};
+
+btnTryAgain.addEventListener("click", tryAgain);
+
+btnNext.addEventListener("click", () => {
+  validate();
+});
+
+window.addEventListener("load", () => {
+  randomQuestion();
+  answerTracker();
+});
